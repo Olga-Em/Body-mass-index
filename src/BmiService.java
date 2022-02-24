37be54dd5bbbe3c0856c4e280a9df95385793958
Olga Em <olga.em020@gmail.com>
2022-02-25 01:16:21 +0700

    Added formatting code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public double calculate (int bodyMass, double height) {
-        double bmi = bodyMass/(height*height);
+    public double calculate(int bodyMass, double height) {
+        double bmi = bodyMass / (height * height);
         return bmi;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-            BmiService service = new BmiService();
-            int bodyMass = 52; //в килограммах
-            double height = 1.6; //в метрах
-            double bmi = service.calculate(bodyMass, height);
-            System.out.println(bmi);
-        }
+        BmiService service = new BmiService();
+        int bodyMass = 52; //в килограммах
+        double height = 1.6; //в метрах
+        double bmi = service.calculate(bodyMass, height);
+        System.out.println(bmi);
     }
+}
